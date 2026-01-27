b863aaf54e13904630648fa15d3560abf974a72b
Zaki Devara <mzakidevara@gmail.com>
2026-01-28 00:46:57 +0700

    init

---
+++ src/main/java/com/devara/paytrans/PaytransApplication.java
@@ -0,0 +1,13 @@
+package com.devara.paytrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytransApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devara/paytrans/PaytransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devara.paytrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
